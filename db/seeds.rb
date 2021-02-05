b5acb904606eaa1be5b3f0ae25e13a7439ac48f5
clairezed <clairezuliani@gmail.com>
2021-02-05 16:30:43 +0100

    Change pole mail

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,13 +29,13 @@ Setting.pole_address = "La ville Cotterel, BP 86048"      if Setting.pole_addres
 Setting.pole_address_complementary = "48 rue de St Malo"  if Setting.pole_address_complementary.blank?
 Setting.pole_city = "35360 Montauban de Bretagne"         if Setting.pole_city.blank?
 Setting.pole_phone = "02 99 06 32 44"                 if Setting.pole_phone.blank?
-Setting.pole_mail = "coordination@broceliande-richesses-associatives.org"                    if Setting.pole_mail.blank?
+Setting.pole_mail = "coordination@ess-broceliande.bzh"                    if Setting.pole_mail.blank?
 Setting.baseline = "Dynamiser les projets d'utilité sociale sur le territoire de Brocéliande"    if Setting.baseline.blank?
 Setting.newsletter_subscription_title = "Inscrivez-vous à notre newsletter"  if Setting.newsletter_subscription_title.blank?
 Setting.newsletter_subscription_description = "Newsletter ? Une info par lettre, soignée et choyée, à échéance régulière dans votre boîte aux lettres"                        if Setting.newsletter_subscription_description.blank?
 Setting.contact_bloc_description = "Vous avez des tonnes de question ? Vous souhaitez mieux identifier ce qui existe sur le territoire ?" if Setting.contact_bloc_description.blank?
 Setting.contact_bloc_button = "Contacter le pôle" if Setting.contact_bloc_button.blank?
-Setting.admin_emails = %w[bonjour@lassembleuse.fr coordination@broceliande-richesses-associatives.org mathilde.herault@broceliande-richesses-associatives.org]    if Setting.admin_emails.blank?
+Setting.admin_emails = %w[bonjour@lassembleuse.fr coordination@ess-broceliande.bzh coordination@broceliande-richesses-associatives.org mathilde.herault@broceliande-richesses-associatives.org]    if Setting.admin_emails.blank?
 
 EmailTemplate.where(mailer: "ParticipantMailer", mail_name: "new_subscription").first_or_create(body: "Le pôle vous recontactera rapidement pour préciser les détails pratiques et le règlement.")
 
